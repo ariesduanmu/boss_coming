65699c11123fff9969618112211ab9314ce10be5
ariesduanmu <aries.duanmu@gmail.com>
2018-11-15 11:43:39 +0800

    recognize face

--- camera.py
+++ camera.py
@@ -35,6 +35,41 @@ def capture_video(output_file="output.avi"):
 def compress_video(file_path):
     pass
 
+def save_face_from_video():
+    print("STRAT")
+    cap = cv2.VideoCapture(0)
+    k = 0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            if k % 3 == 0:
+
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                if len(face_locations) > 0:
+                    print(f"[*] {k} | with face: {len(face_locations)}")
+                for top, right, bottom, left in face_locations:
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
+
+                    cropped = frame[left:right, top:bottom]
+                    # cv2.imshow("cropped", cropped)
+                    cv2.imwrite("images/image_{}.jpg".format(k), cropped)
+            k += 1
+            cv2.imshow('Video', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def split_video2flames(file_path="output.avi"):
     vid = mpy.VideoFileClip(file_path)
     for frame in vid.iter_frames(dtype="uint8"):
@@ -44,7 +79,6 @@ def save_frame(frame, output):
     image = Image.fromarray(frame)
     image.save(output)
         
-
 def locate_face(image, output_file):
     face_locations = face_recognition.face_locations(image)
     if len(face_locations) > 0:
@@ -62,9 +96,15 @@ def gather_face_data(image):
 
 
 if __name__ == "__main__":
-    dst = "my_images"
-    i = 0
-    for frame in split_video2flames("video/video_2.mov"):
-        save_frame(frame, os.path.join(dst, "me_{:02d}.jpg".format(i)))
-        i += 1
+    # dst = "my_images"
+    # i = 0
+    # for frame in split_video2flames("video/video_2.mov"):
+    #     save_frame(frame, os.path.join(dst, "me_{:02d}.jpg".format(i)))
+    #     i += 1
+
+    # save_face_from_video()
+
+    
+
+
 
---
+++ recognize_face.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from PIL import Image
+import face_recognition
+
+MAX_PIXEL = 256
+IMAGE_WIDTH = 64
+
+def resize_ratio(w, h):
+    if max(w, h) <= MAX_PIXEL:
+        return 1
+    else:
+        if w >= h:
+            return w // MAX_PIXEL
+        else:
+            return h // MAX_PIXEL
+
+def resize_image_cv(frame, width=IMAGE_WIDTH):
+    def get_padding_size(image):
+        h, w, _ = image.shape
+        longest_edge = max(h, w)
+        top, bottom, left, right = (0, 0, 0, 0)
+        if h < longest_edge:
+            dh = longest_edge - h
+            top = dh // 2
+            bottom = dh - top
+        elif w < longest_edge:
+            dw = longest_edge - w
+            left = dw // 2
+            right = dw - left
+        else:
+            pass
+        return top, bottom, left, right
+
+    top, bottom, left, right = get_padding_size(frame)
+    BLACK = [0, 0, 0]
+    constant = cv2.copyMakeBorder(frame, top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK)
+
+    resized_image = cv2.resize(constant, (width, width))
+
+    return resized_image
+
+def resize_image_pil(frame, width=IMAGE_WIDTH):
+    w, h = frame.size
+    s = max(w, h)
+    resized_image = Image.new("RGB", (s, s))
+    resized_image.paste(frame, ((s-w)//2, (s-h)//2))
+    resized_image.thumbnail((width,width))
+
+    return resized_image
+
+def recognize_face(frame, ratio):
+    face_locations = face_recognition.face_locations(frame)
+    for top, right, bottom, left in face_locations:
+        yield (top*ratio, right*ratio, bottom*ratio, left*ratio)
+
+def recognize_face_from_image_cv(image_path, output_path, resize=False):
+    frame = cv2.imread(image_path)
+    h, w, k = frame.shape
+    ratio = resize_ratio(w, h)
+    small_frame = cv2.resize(frame, (0, 0), fx=1/ratio, fy=1/ratio)
+    small_frame = small_frame[:, :, ::-1]
+    for top, right, bottom, left in recognize_face(small_frame, ratio):
+        cropped = frame[left:right, top:bottom]
+        if resize:
+            cropped = resize_image_cv(cropped)
+        cv2.imwrite(output_path, cropped)
+
+def recognize_face_from_image_pil(image_path, output_path, resize=False):
+    frame = Image.open(image_path)
+    w, h = frame.size
+    ratio = resize_ratio(w, h)
+    small_frame = frame.resize((w//ratio, h//ratio))
+    small_frame = np.array(small_frame)
+    for top, right, bottom, left in recognize_face(small_frame, ratio):
+        cropped = frame.crop((left,top,right,bottom))
+        if resize:
+            cropped = resize_image_pil(cropped)
+        cropped.save(output_path)
+
+if __name__ == "__main__":
+    recognize_face_from_image_cv("lmq.jpg", "images/image.jpg", True)
+
+
